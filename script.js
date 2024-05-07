f66d600e330122435ab8ea117c3c274321c07d52
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2024-05-07 15:30:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const listaPokemon = document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
 let URL =  "https://pokeapi.co/api/v2/pokemon/";
 
-for  (let i = 1; 1 <= 151; i++) {
+for  (let i = i; i <= 151; i++) {
     fetch(URL + i)
         .then((response) =>  response.json())
         .then(data => mostrarPokemon(data))  
